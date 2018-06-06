c3df02eb87c910718398baaaf03f74ef3dfec2da
axj1l59 <adam_johnston1@homedepot.com>
2018-06-06 17:36:24 -0400

    WIP new redis code

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 require('dotenv').config()
-import { redis } from './redis'
+import { publisher as publisherCreator, subscriber as subscriberCreator } from './redis'
 import slack from 'slack'
 
 import { initalizeSlack } from './slack'
 
 console.log('---->', process.env.SLACK_OAUTH_TOKEN)
 
-const { publish, subscribe } = redis()
-initalizeSlack({
-	slack,
-	token: process.env.SLACK_OAUTH_TOKEN,
-	publish,
-	subscribe
+Promise.all([
+	publisherCreator(),
+	subscriberCreator()
+])
+.then(([
+	{ publish },
+	{ subscribe }
+]) => {
+	initalizeSlack({
+		slack,
+		token: process.env.SLACK_OAUTH_TOKEN,
+		publish,
+		subscribe
+	})
 })
--- src/redis/index.js
+++ src/redis/index.js
@@ -1,140 +1,102 @@
-// Basically have listeners, publishers, and actions
-// Need some sort of naming convention
-
-import { createClient } from 'redis'
+import { createClient, ReplyError } from 'redis'
 import { createSubject } from 'create-subject-with-filter'
-
-const providerConfigParams = {
-	getPortParams: []
-}
-
-// {
-// 	channel,
-// 	providerConfig: {
-// 		pubsub: {
-// 			getPort
-// 		}
-// 	}
-// }
 
 const clients = {} // inject this to manage scope
 
-const getClient = ({ type }) => ({
-	connect: () => new Promise(resolve => {
-		if (!clients[type]) {
-			setClient({
-				type,
-				client: createClient({
-					retry_strategy: function (options) {
-						console.log('options', options)
-						if (options.error && options.error.code === 'ECONNREFUSED') {
-						// End reconnecting on a specific error and flush all commands with
-						// a individual error
-							if (options.attempt >= 20) {
-							    // End reconnecting with built in error
-							    return undefined;
-							}
-							return Math.min(options.attempt * 100, 5000);
-						}
-						// reconnect after
-						return Math.min(options.attempt * 100, 5000);
-					},
-					host: process.env.IP_ADDRESS,
-					port: 6379
-				})
-			})
-		}
-		return resolve(clients[type])
-	})
-})
-
-const setClient = ({ type, client }) =>  clients[type] = client
-
-export const redis = () => ({
-	publish: () => new Promise((resolve, reject) => {
-		const c = getClient({ type: '__publisher' })
-		return resolve({
-			connect: () => c.connect().then(client => ({
-				send: ({
-					channel,
-					data
-				}) => new Promise(resolve => {
-					client.on('error', (...args) => {
-						console.log('publish - error', args)
-					})
-					client.publish(channel, JSON.stringify(data))
-					return resolve({
-						meta: {
-							type: 'published',
-							timestamp: new Date().getTime()
-						}
-					})
-				})
-			}))
-		})
-	}),
-	subscribe: ({ channel }) => new Promise(resolve => {
-		const c = getClient({ type: channel })
-		return resolve({
-			connect: () => c.connect()
-				.then(client => {
-					const {
-						subscribe: allMsgs,
-						filter: filterMsgs,
-						next
-					} = createSubject()
-					client.subscribe(channel)
-					client.on('error', (...args) => {
-						console.log('subscribe - error', args)
-					})
-					client.on('connect', (...args) => {
-						console.log('Connected to Redis')
-						// ...args looks like [ 'motion sensor', '{"msg":{"motion":false}}' ]
-						client.on('message', (...args) => {
-							// console.log('args', args)
-							next({
-								meta: {
-									type: 'message',
-									timestamp: new Date().getTime()
-								},
-								data: args //sanitize anything provider specific (redis)
-							})
-						})
-						client.on('error', (...args) => {
-							console.log('ERROR OCCURRED', error)
-							next({
-								meta: {
-									type: 'error',
-									timestamp: new Date().getTime(),
-									data: args //sanitize anything provider specific (redis)
+const getClient = ({ type }) => {
+	return {
+		connect: () => new Promise(resolve => {
+			// console.log('------>', clients)
+			if (!clients[type]) {
+				console.log('Creating new client')
+				setClient({
+					type,
+					client: createClient({
+						retry_strategy: function (options) {
+							console.log('options', options)
+							if (options.error && options.error.code === 'ECONNREFUSED') {
+							// End reconnecting on a specific error and flush all commands with
+							// a individual error
+								if (options.attempt >= 20) {
+									// End reconnecting with built in error
+									return undefined;
 								}
-							})
-						})
-						// console.log('asdfda connection')
-						next({
-							meta: {
-								type: 'connect',
-								timestamp: new Date().getTime(),
-								data: args //sanitize anything provider specific (redis)
+								return Math.min(options.attempt * 100, 5000);
 							}
-						})
+							// reconnect after
+							return Math.min(options.attempt * 100, 5000);
+						},
+						host: process.env.IP_ADDRESS,
+						port: 6379
 					})
-					return {
-						allMsgs,
-						filterMsgs
+				})
+			}
+			return resolve(clients[type])
+		})
+	}
+}
+
+const setClient = ({ type, client }) => clients[type] = client
+
+// This should only be used as a singleton
+// Calling this multiple times will open up more `client.on('error')` listeners
+export const publisher = () => new Promise(resolve => {
+	const { connect } = getClient({ type: 'publisher' })
+	return connect().then(client => {
+		client.on('error', (...args) => {
+			console.log('publish - error', args)
+		})
+		return resolve({
+			publish: ({
+				channel,
+				data
+			}) => {
+				data = JSON.stringify(data)
+				client.publish(channel, data)
+				return resolve({
+					meta: {
+						type: 'published',
+						timestamp: new Date().getTime()
 					}
 				})
+			}
 		})
 	})
 })
 
-
-export const doRedisReconnect = ({ err, delay }) => new Promise((resolve, reject) => {
-	// let attempts = 0
-	// if ()
-	// console.log('HELLO')
-	setTimeout(() => {
-		console.log('Attempting to reconnect')
-		resolve({ err })
-	}, 5000)
+export const subscriber = () => new Promise(resolve => {
+	const { connect } = getClient({ type: 'subscriber' })
+	return connect().then(client => {
+		const {
+			subscribe: allMsgs,
+			filter: filterMsgs,
+			next
+		} = createSubject()
+		client.on('error', (...args) => {
+			console.log('subscriber - error', args)
+		})
+		client.on('connect', (...args) => {
+			console.log('Connected to Redis')
+			// ...args looks like [ 'motion sensor', '{"msg":{"motion":false}}' ]
+			client.on('message', (...args) => {
+				next({
+					meta: {
+						type: 'message',
+						timestamp: new Date().getTime()
+					},
+					data: args
+				})
+			})
+		})
+		return resolve({
+			subscribe: ({
+				channel
+			}) => new Promise(resolve => {
+				return resolve({
+					allMsgs,
+					filterMsgs
+				})
+			})
+		})
+	})
 })
